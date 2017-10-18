8a4177457bdefc3eb708b67abbddc511afcd5086
Ben Hall <benji2357@gmail.com>
2017-10-18 22:25:08 +0100

    fix: Relaxed new-parens and whitespace rules

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 module.exports = {
   extends: ['tslint-config-standard'],
   rules: {
+    "new-parens": false,
     "trailing-comma": [true, {"multiline": "always", "singleline": "never"}],
+    "whitespace": [
+      true,
+      "check-branch",
+      "check-decl",
+      "check-operator",
+      "check-type",
+      "check-typecast",
+      "check-preblock"
+    ]
   }
 }
